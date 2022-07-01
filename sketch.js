bebe97256858feccaf84c3757f4b0639ea82d179
henrique-abacate <97639444+henrique-abacate@users.noreply.github.com>
2022-07-01 19:59:35 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,1 +1,37 @@
-var parede
+var parede;
+var player;
+var inimigo;
+var xplayer= 400;
+var yplayer= 400;
+var xinimigo= 300;
+var yinimigo= 300;
+
+
+
+function setup (){
+    createCanvas (1200,600)
+    player = createSprite (xplayer,yplayer,10,10);
+inimigo = createSprite (xinimigo,yinimigo,10,10);
+}
+function draw(){
+background("white")
+if(keyDown ("RIGHT") ){
+player.x+= 2;
+}
+
+if(keyDown ("LEFT") ){
+player.x -=2;
+}
+
+if(keyDown ("UP") ){
+player.y-=2;
+}
+
+if(keyDown ("DOWN") ){
+player.y+=2;
+}
+
+drawSprites ()
+
+
+}
